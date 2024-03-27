64db2bd65515f0ce960d705200072269bde80c64
devhak <devhak.enzo@gmail.com>
2024-03-27 23:25:21 +0900

    getUser - redis 조회 코드 추가

--- jediscache/src/main/java/com/example/jediscache/service/UserService.java
+++ jediscache/src/main/java/com/example/jediscache/service/UserService.java
@@ -5,18 +5,29 @@ import com.example.jediscache.UserRepository;
 import com.example.jediscache.domain.entity.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.beans.Transient;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RedisTemplate<String, User> userRedisTemplate;
 
     @Transactional
     public User getUser(final Long id){
-        return userRepository.findById(id).orElseThrow();
+        var key = "users:%d".formatted(id);
+        var cacheUser = userRedisTemplate.opsForValue().get(key);
+
+        if(cacheUser != null){
+            return cacheUser;
+        }
+        User user = userRepository.findById(id).orElseThrow();
+
+        userRedisTemplate.opsForValue().set(key, user);
+
+        return user;
     }
 }
