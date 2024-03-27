8ec1c71643959a7afdc93fb22b9956a295f4ebca
devhak <devhak.enzo@gmail.com>
2024-03-27 22:20:36 +0900

    redistemplate  config 생성

---
+++ jediscache/src/main/java/com/example/jediscache/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.jediscache.config;
+
+import com.example.jediscache.domain.entity.User;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory connectionFactory){
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+
+        var template = new RedisTemplate<String, User>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, User.class));
+        return template;
+
+    }
+}
